691546d72c69d90d425561b3f75d113dfa60cad9
thomas-meyer-57647 <thomas.meyer.57647@gmail.com>
2022-08-25 02:09:38 +0200

    initialize commit

---
+++ Hurricane/src/main/java/com/tmt/hurricane/HurricaneApplication.java
@@ -0,0 +1,13 @@
+package com.tmt.hurricane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HurricaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HurricaneApplication.class, args);
+	}
+
+}
---
+++ Hurricane/src/test/java/com/tmt/hurricane/HurricaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmt.hurricane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HurricaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
